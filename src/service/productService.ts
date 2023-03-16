3753a49271d67bafe992f38ea97a96ccff37fb1d
matheus <matheusmarquesneves15@gmail.com>
2023-03-16 19:15:40 -0300

    forEach em vez de map

--- src/service/productService.ts
+++ src/service/productService.ts
@@ -18,7 +18,7 @@ class ProductService {
     public async findStock() {
         const getProducts = await this.findProduct()
         const productsStock: Array<object> = []
-        getProducts.map((product) => productsStock.push({ "nome": product.nome, "qtde": product.qtde, "preco": product.preco ,"valor_estoque": product.qtde * product.preco}))
+        getProducts.forEach((product) => productsStock.push({ "nome": product.nome, "qtde": product.qtde, "preco": product.preco ,"valor_estoque": product.qtde * product.preco}))
         return productsStock
     }
 }
