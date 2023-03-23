e281a3ebe2549e1e7b1d695f96373da6b4494526
Matheus Marques <81122374+MatheusMarques184@users.noreply.github.com>
2023-03-23 20:45:27 -0300

    lendo arquivo pokemon e criando uma pokedex

--- src/atividades/pokeapi/index.ts
+++ src/atividades/pokeapi/index.ts
@@ -1,14 +1,21 @@
-import { writeFile } from 'fs/promises'
+import { writeFile, readFile, appendFile } from 'fs/promises'
 
 class pokeApi {
 
     public async getPokemon() {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2")
         .then((response) => response.json())
-        .then((data) =>  writeFile('pokedex.json', JSON.stringify(data.results, null, 2)));
+        .then((data) =>  writeFile('pokemons.json', JSON.stringify(data.results, null, 2)));
     }
 
+    public async getPokemonLocal() {
+        const pokedex = JSON.parse(await readFile('pokemons.json', "utf-8"))
+        pokedex.forEach(async (i) => {
+            await fetch(i.url).then((response) => response.json())
+                              .then((data) => appendFile('pokedex.json', JSON.stringify(data, null, 2)))
+                        })
+    }
 }
 
 const exec = new pokeApi()
-exec.getPokemon()
+exec.getPokemonLocal()
